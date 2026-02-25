c33dbe8ce2bc0ef223351c3188a3ab184e1bb3f1
Gerardo Robert <gerobzel@gmail.com>
2026-02-25 16:52:18 -0300

    fix: destroy ECR

--- terraform/shared-infra/ecr/ecr.tf
+++ terraform/shared-infra/ecr/ecr.tf
@@ -2,6 +2,9 @@ resource "aws_ecr_repository" "main" {
   name                 = "${var.project}/${var.app_name}"
   image_tag_mutability = "MUTABLE"
 
+  # In production environments this should be false to prevent accidental image loss.
+  force_delete = true
+
   image_scanning_configuration {
     scan_on_push = true
   }
