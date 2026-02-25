1de9d9f7774d265917be4fd8069a621030923ded
Gerardo Robert <gerobzel@gmail.com>
2026-02-25 15:48:26 -0300

    feat: new message on port 80

--- apps/hello-world/src/main.py
+++ apps/hello-world/src/main.py
@@ -5,7 +5,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class HelloWorldHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        body = b"Hello World"
+        body = b"Hello Streaver!!"
         self.send_response(200)
         self.send_header("Content-Type", "text/plain")
         self.send_header("Content-Length", str(len(body)))
