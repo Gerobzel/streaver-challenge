bf30dc9c66c5894d039f04abd34b619cebad5666
Gerardo Robert <gerobzel@gmail.com>
2026-02-25 18:27:46 -0300

    fix: error code

--- apps/hello-world/src/main.py
+++ apps/hello-world/src/main.py
@@ -22,7 +22,7 @@ class HealthCheckHandler(BaseHTTPRequestHandler):
             with socket.create_connection(("localhost", 80), timeout=2):
                 code, body = 200, b"Service Healthy"
         except OSError:
-            code, body = 403, b"Service Unavailable"
+            code, body = 503, b"Service Unavailable"
         self.send_response(code)
         self.send_header("Content-Type", "text/plain")
         self.send_header("Content-Length", str(len(body)))
